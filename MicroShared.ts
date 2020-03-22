6f25180deaaccc6c188be31b6e297856942f3d62
Jake Savage <website-repos@peek.com>
2020-03-22 10:09:19 -0600

    added set segmentEffect and several segment modifying functions

--- MicroShared.ts
+++ MicroShared.ts
@@ -4,8 +4,11 @@ import {WebMicroInfo, ControllerMicroSegment,
 import {MicroEffect, WebEffect} from './MicroCommands';
 export class SharedMicroState {
   microState: WebMicroInfo;
+  segmentBoundaries: number[];
   constructor(microInfo: WebMicroInfo) {
     this.microState = microInfo;
+    this.segmentBoundaries = this.calculateSegmentBoundaries();
+
   }
   getSegments = () => {
     return this.microState.segments;
@@ -15,6 +18,9 @@ export class SharedMicroState {
   }
   setBrightness = (brightness: number) => {
     this.microState.brightness = brightness;
+  }
+  setEffect(index: number, effect: WebEffect) {
+    this.getSegment(index).effect = effect;
   }
   getTotalLEDs = () => {
     return this.microState.totalLEDs;
@@ -30,6 +36,47 @@ export class SharedMicroState {
   }
   getId = () => {
     return this.microState.id;
+  }
+  getSegment = (index: number) => {
+    return this.getSegments()[index];
+  }
+  resizeSegment = (index: number, offset: number, numLEDs: number) => {
+    const segment = this.getSegment(index);
+    segment.offset = offset;
+    segment.numLEDs = numLEDs;
+  }
+  getSegmentBoundaries = () => {
+    return this.segmentBoundaries;
+  }
+  setSegmentBoundaries = (segmentBoundaries: number[]) => {
+    this.segmentBoundaries = segmentBoundaries;
+  }
+  resizeSegmentsFromBoundaries = (boundaries: number[]) => {
+    const {
+      getSegmentCount, resizeSegment,
+      setSegmentBoundaries
+    } = this;
+    const iterations = getSegmentCount();
+    for(let i = 0; i < iterations; i++){
+      const [offset, segmentEnd] = boundaries.slice(i, i+2);
+      const numLEDs = segmentEnd - offset;
+      resizeSegment(i, offset, numLEDs);
+    }
+    setSegmentBoundaries(boundaries);
+  }
+  calculateSegmentBoundaries = () => {
+    const boundaries: number[] = this.getSegments()
+      .reduce((boundaries, segment, index) => {
+        if (index === 0) {
+          boundaries.push(0);
+          boundaries.push(segment.numLEDs);
+        }else {
+          boundaries
+            .push(segment.offset + segment.numLEDs);
+        }
+        return boundaries;
+      }, [] as number[]);
+    return boundaries;
   }
 }
 export class Convert {
