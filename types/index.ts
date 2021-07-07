5d102ceada71c6e31b893ab7ecb59b7377a6fbd8
Jake Savage <jxsavage@msn.com>
2021-07-07 17:38:14 -0600

    Added Plasma Effect.

--- types/index.ts
+++ types/index.ts
@@ -18,7 +18,8 @@ export enum MicroEffect {
   BlendWave,
   BeatWave,
   Blur,
-  Confetti
+  Confetti,
+  Plasma
 }
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const POSSIBLE_EFFECTS_STRINGS = Object.values(MicroEffect).filter((k) => typeof MicroEffect[k as any] === 'number');
